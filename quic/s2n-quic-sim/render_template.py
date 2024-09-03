ac40f1c2a51d1042f64cd62ab95f82837b527a7e
LouisLU9911 <louislu9911@gmail.com>
2024-09-03 03:54:11 +0100

    feat: format after rendering

--- quic/s2n-quic-sim/render_template.py
+++ quic/s2n-quic-sim/render_template.py
@@ -7,16 +7,16 @@ from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 from tqdm import tqdm
 import argparse
+import os
+import pandas as pd
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Run batch processes with multiple seeds.")
-parser.add_argument(
-    "--seeds", type=str, required=True, help="Comma-separated list of seeds."
-)
+parser.add_argument('--seeds', type=str, required=True, help="Comma-separated list of seeds.")
 args = parser.parse_args()
 
 # Convert the comma-separated seeds into a list of integers
-seeds = [int(seed) for seed in args.seeds.split(",")]
+seeds = [int(seed) for seed in args.seeds.split(',')]
 
 # Define the directory and template name
 template_dir = Path(__file__).parent / "plans"
@@ -45,7 +45,98 @@ iterations = 1
 streams = 5
 stream_data = 500000000
 
-# Loop through each seed
+columns = [
+    "event",
+    "timestamp",
+    "lost_bytes",
+    "bytes_acknowledged",
+    "bytes_in_flight",
+    "congestion_window",
+]
+
+CWD = os.getcwd()
+reports_dir_tmpl = "reports_seed_{}"
+report_dir_tmpl = "delay_{}_drop_{}"
+
+def get_all_lines_from_report_dir(report_dir) -> list:
+    filtered_lines = []
+    if report_dir.is_dir():
+        stderr_path = report_dir / "stderr.log"
+        print(f"Reading {stderr_path}...")
+        if stderr_path.exists():
+            # Read the stderr.log file
+            with open(stderr_path, "r", encoding="utf-8") as file:
+                lines = file.read().split("\n")
+            # Keep only lines that start with "event:"
+            filtered_lines = [line for line in lines if line.startswith("event:")]
+    return filtered_lines
+
+def filter_out(lines: list, event: str) -> list:
+    filtered_lines = [line for line in lines if event not in line]
+    return filtered_lines
+
+def format_line(line):
+    if "on_packet_sent" in line:
+        cols = line.split(",")
+        record = {
+            columns[0]: cols[0].split(":")[1],
+            columns[1]: cols[1].split(":", 1)[1],  # time_sent
+            columns[2]: 0,
+            columns[3]: 0,
+            columns[4]: cols[3].split(":")[1],
+            columns[5]: cols[4].split(":")[1],
+        }
+    elif "on_packet_lost" in line:
+        cols = line.split(",")
+        record = {
+            columns[0]: cols[0].split(":")[1],
+            columns[1]: cols[1].split(":", 1)[1],  # timestamp
+            columns[2]: cols[3].split(":")[1],  # lost_bytes
+            columns[3]: 0,
+            columns[4]: cols[6].split(":")[1],
+            columns[5]: cols[7].split(":")[1],
+        }
+    elif "on_ack" in line:
+        cols = line.split(",")
+        record = {
+            columns[0]: cols[0].split(":")[1],
+            columns[1]: cols[4].split(":", 1)[1],  # ack_receive_time
+            columns[2]: 0,
+            columns[3]: cols[3].split(":")[1],  # bytes_acknowledged
+            columns[4]: cols[5].split(":")[1],
+            columns[5]: cols[6].split(":")[1],
+        }
+    else:
+        raise Exception(f"Unsupported line: {line}")
+    return record
+
+def ts2ms(timestamp: str) -> int:
+    h, m, s = timestamp.split(":")
+    m = int(m) + int(h) * 60
+    ms = round(float(s) * 1000) + m * 60 * 1000
+    return ms
+
+def format_stderr(seed, delay, drop_rate, save=False):
+    # Define the path to the reports directory
+    reports_dir = Path(CWD) / reports_dir_tmpl.format(seed)
+    report_dir = reports_dir / report_dir_tmpl.format(delay, drop_rate)
+
+    filtered_lines = get_all_lines_from_report_dir(report_dir)
+    filtered_lines = filter_out(filtered_lines, "on_rtt_update")
+    records = list(map(format_line, filtered_lines))
+    df = pd.DataFrame.from_records(records)
+    df["timestamp"] = df["timestamp"].apply(ts2ms)
+    df_one_hot = pd.get_dummies(df, columns=["event"], dtype=int)
+    # Specify the column to move
+    col_to_move = "congestion_window"
+    # Move the column to the end
+    df_one_hot = df_one_hot[
+        [col for col in df_one_hot if col != col_to_move] + [col_to_move]
+    ]
+    if save:
+        df_one_hot.to_csv(report_dir / "formatted.csv", index=False)
+    return df_one_hot
+
 for seed in seeds:
     # Create a directory for reports if it doesn't exist
     reports_dir = Path(__file__).parent / f"reports_seed_{seed}"
@@ -110,3 +201,4 @@ for seed in seeds:
             tqdm.write(
                 f"Process finished for: {output_path} with stderr logged in {stderr_filename}"
             )
+        format_stderr(seed, delay, drop_rate, save=True)
